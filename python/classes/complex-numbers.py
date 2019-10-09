032a667640eb1675d38cd41195029c226424c0fd
pkdism <pawan.dwivedi94@gmail.com>
2019-10-09 15:23:42 +0530

    python classes

---
+++ python/classes/complex-numbers.py
@@ -0,0 +1,54 @@
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __add__(self, no):
+        real = self.real + no.real
+        imaginary = self.imaginary + no.imaginary
+        return Complex(real, imaginary)
+
+    def __sub__(self, no):
+        real = self.real - no.real
+        imaginary = self.imaginary - no.imaginary
+        return Complex(real, imaginary)
+
+    def __mul__(self, no):
+        real = self.real * no.real - self.imaginary * no.imaginary
+        imaginary = self.imaginary * no.real + self.real * no.imaginary
+        return Complex(real, imaginary)
+
+    def __truediv__(self, no):
+        den = pow(no.real, 2) + pow(no.imaginary, 2)
+        no.imaginary *= (-1)
+        prod = (Complex(self.real, self.imaginary) * Complex(no.real, no.imaginary))
+        real = prod.real/den
+        imaginary = prod.imaginary/den
+        return Complex(real, imaginary)
+
+    def mod(self):
+        val = pow(pow(self.real, 2) + pow(self.imaginary, 2), 0.5)
+        return Complex(round(val, 2), 0)
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
