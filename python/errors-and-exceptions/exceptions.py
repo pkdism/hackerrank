ebfde284d4ae84c007b9fbb9b384d0e64e1f1150
pkdism <pawan.dwivedi94@gmail.com>
2019-10-09 12:04:39 +0530

    python errors and exceptions

---
+++ python/errors-and-exceptions/exceptions.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+    try:
+        a, b = map(int, input().split())
+        print(a//b)
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+    except ValueError as e:
+        print("Error Code:", e)
